c8b6ad94a6cf771262530c87d545b0870f8ee166
Shishkanov_Andrey <goodzon91@gmail.com>
2018-08-15 21:53:14 +0300

    perfomance fix

--- src/app/shared/modules/validation/validation.component.ts
+++ src/app/shared/modules/validation/validation.component.ts
@@ -3,25 +3,33 @@ import { FormControl } from '@angular/forms';
 
 @Component({
     selector: 'validation-messages',
-    template: `<div *ngIf="this.control.touched && this.control.invalid" class="invalid-feedback">{{errorMessage}}</div>`
+    template: `<div *ngIf="this.formControl.touched && this.formControl.invalid" class="invalid-feedback">{{errorMessage}}</div>`
 })
 export class ValidationComponent {
     config = {
         'required': 'This field is required!'
     };
-    @Input() control: FormControl;
+    @Input()
+    set control(value: FormControl) {
+        this.formControl = value;
+        this.formControl.statusChanges.subscribe(() => {
+            if (this.formControl.errors){
+                this.errorMessage = this.getValidatorErrorMessage(Object.keys(this.formControl.errors)[0]);
+            }
+        });
+    }
+    
+    formControl: FormControl;
+    
+    errorMessage: string;
+    
     constructor() { }
-
+    
     getValidatorErrorMessage(validatorName: string) {
-        if(this.config.hasOwnProperty(validatorName)){
+        if (this.config.hasOwnProperty(validatorName)){
             return this.config[validatorName];
         }else{
             return validatorName;
         }
-    }
-
-    get errorMessage() {
-        if (!this.control.errors) return null;
-        return this.getValidatorErrorMessage(Object.keys(this.control.errors)[0]);
     }
 }
